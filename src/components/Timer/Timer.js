f35e26a53196d0a2cd28578788a90ecfb75a4c03
Natalia Jaguś <natalia.jagus@gmail.com>
2019-04-26 18:10:48 +0200

    change Timer to class component

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './Timer.module.css';
 
-const Timer = () => (
-    <div className={styles.container}>
-        <span className={styles.time}>02:29</span>
-        <span className={styles.mode}>bieg</span>
-    </div>
-);
+class Timer extends Component {
+
+    state = {
+
+    }
+
+    render () {
+        <div className={styles.container}>
+            <span className={styles.time}>02:29</span>
+            <span className={styles.mode}>bieg</span>
+        </div>
+    }
+
+    componentDidMount() {
+        
+    }
+
+    convertSeconds = (s) => {
+        var min = Math.floor(s / 60);
+        var sec = s % 60;
+        if (min < 10 && sec < 10) {
+            return '0' + min + ':0' + sec;
+        } else if (min > 10 && sec < 10) {
+            return min + ':0' + sec;
+        } else if (min < 10 && sec >= 10) {
+            return '0' + min + ':' + sec;
+        } else {
+            return min + ':' + sec;
+        }
+    }
+
+}
 
 export default Timer;
